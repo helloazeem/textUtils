e3a0dbeee1b4aad5a54f6938b2859df29aa1f8ba
helloazeem <helloajimul@gmail.com>
2022-07-30 19:32:59 +0600

    Writeable form and convert uppercase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navber from './Components/Navber';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
     <Navber title="TextUtils"/>
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze"/>
+    </div>
     </>
   );
 }
